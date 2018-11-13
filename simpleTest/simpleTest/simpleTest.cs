53eb81bd9a29167e82fdf13e19556a58cb13bbd7
Roman <Roman@MacBook-Air-13-Roman.local>
2018-11-13 22:20:52 +0300

    new files for adding

---
+++ simpleTest/simpleTest/simpleTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+namespace simpleTest
+{   
+    [TestFixture]
+    public class simpleTest
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        [Test]
+        public void simpleTest1()
+        {
+            var a = 1;
+            var b = 3;
+            Assert.Greater(b, a);
+        }
+        [Test]
+        public void simpleTest2()
+        {
+            var a = 3;
+            var b = 2.95;
+            var deviation = 0.1;
+            Assert.AreEqual(a, b, deviation, 
+                            $"Deviation is more than {deviation}!");
+        }
+
+    }
+}
