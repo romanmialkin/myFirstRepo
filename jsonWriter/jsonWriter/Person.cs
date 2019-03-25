9006df164032dc016ac2fdd2b7d0eca252f4281c
Roman <Roman@MacBook-Air-13-Roman.local>
2019-03-25 22:27:39 +0300

    jsonWriter was added

---
+++ jsonWriter/jsonWriter/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace jsonWriter
+{
+    public enum PersonType
+    {
+        Client,
+        Administrator,
+        Specialist
+    }
+
+
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public PersonType PersonType { get; set; }
+
+        protected Person(int id, string lastName,
+            string name, PersonType personType)
+        {
+            PersonType = personType;
+            Id = id;
+            LastName = lastName;
+            Name = name;
+        }
+
+        public static Person GetPersonById(int id, List<Person> persons)
+        {
+            foreach (var person in persons)
+            {
+                if (id == person.Id)
+                    return person;
+            }
+            return null;
+        }
+    }
+}
---
+++ jsonWriter/jsonWriter/Specialist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace jsonWriter
+{
+    public enum SpecialistType
+    {
+        Doctor,
+        Diagnostician,
+        Nurse
+    }
+
+    public abstract class Specialist : Person
+    {
+        public int SubId { get; set; }
+        public SpecialistType SpecialistType { get; set; }
+        public string RoomNumber { get; set; }
+
+        public Specialist(int id, string lastName,
+            string name, int subId, SpecialistType specialistType, 
+            string roomNumber, PersonType personType = PersonType.Specialist) 
+            : base(id, lastName, name, personType)
+        {
+            SubId = subId;
+            SpecialistType = specialistType;
+            RoomNumber = roomNumber;
+        }
+    }
+}
