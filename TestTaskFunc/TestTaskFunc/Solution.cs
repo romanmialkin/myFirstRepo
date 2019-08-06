b05f9c7f1c0263ea6e8e170674d8ab556fdc3b7a
Roman Mialkin <roman.mialkin@simtech.spb.ru>
2019-08-06 19:33:47 +0300

    TestTask was added

---
+++ TestTaskFunc/TestTaskFunc/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+namespace TestTaskFunc
+{
+    [TestFixture]
+    public class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var newNums = nums.Select((n, i) => Tuple.Create(n, i)).OrderBy(t => t.Item1);
+            
+
+            var indexes = new int[2];
+            return  indexes;
+        }
+
+
+       
+
+        
+
+        [Test]
+        public static void Run1()
+        {
+            var numbers = new[] {1, 4, 7, 8, 12, 9, 16};
+            var target = 10;
+            var controlIndexex = new[] {0, 5};
+            
+            Assert.That(controlIndexex ==TwoSum(numbers, target));
+        }
+
+    }
+}
